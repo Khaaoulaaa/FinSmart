2f1dbbac89dc512e98cdb16bfa3f4027f7220e56
Khaaoulaaa <khaoulablog123@gmail.com>
2026-05-28 14:44:19 +0200

    feat: add demo data models and seed script

--- database.py
+++ database.py
@@ -1,6 +1,7 @@
 from collections.abc import Generator
 from time import sleep
 
+from sqlalchemy.exc import OperationalError
 from sqlalchemy import create_engine
 from sqlalchemy.exc import OperationalError
 from sqlalchemy.orm import DeclarativeBase, Session, sessionmaker
